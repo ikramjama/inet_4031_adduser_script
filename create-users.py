6477d1999c869cb8a642db4200db3d7845f71a67
ikramjama <157758981+ikramjama@users.noreply.github.com>
2024-02-18 23:31:22 -0600

    Create-users.py

--- create-users.py
+++ create-users.py
@@ -3,24 +3,32 @@ import os
 import re
 import sys
 def main():
+    #loop through each line of input
     for line in sys.stdin:
-        match = re.match("^#",line)
+        # Check if the line starts with a '#' indicating a comment
+        match = re.match("^#", line)
+        # Split the line into fields using ':' as delimiter
         fields = line.strip().split(':')
+        #skip lines starting with '#' or those without 5 feilds
         if match or len(fields) != 5:
             continue
+        #take out username, password, gecos, and groups from fields
         username = fields[0]
         password = fields[1]
         gecos = "%s %s,,," % (fields[3], fields[2])
         groups = fields[4].split(',')
-        print( "==> Creating account for %s..." % (username))
+        #creating user account
+        print("==> Creating account for %s..." % (username))
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos, username)
         os.system(cmd)
-        print ("==> Setting the password for %s..." % (username))
+        #make password for the user
+        print("==> Setting the password for %s..." % (username))
         cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password, password, username)
-        os.system(cmd)
+        os.system(cmd) 
+        #assign user to specified groups
         for group in groups:
             if group != '-':
-                print( "==> Assigning %s to the %s group..." % (username, group))
+                print("==> Assigning %s to the %s group..." % (username, group))
                 cmd = "/usr/sbin/adduser %s %s" % (username, group)
                 os.system(cmd)
 
